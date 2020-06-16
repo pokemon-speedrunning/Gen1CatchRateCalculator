2cd49858172fe7b54f7a76b8944b40ada2aec7a1
TiKevin83 <travismcgeehan@gmail.com>
2020-06-15 22:03:39 -0400

    code cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-var pokeballs = {pokeBall : {ballFactor: 12, reroll1: false, reroll2: false, ballRerollCutoff: 256},
-    greatBall: {ballFactor: 8, reroll1: true, reroll2: false, ballRerollCutoff: 201},
-    ultraBall: {ballFactor: 12, reroll1: true, reroll2: true, ballRerollCutoff: 151},
-    safariBall: {ballFactor: 12, reroll1: true, reroll2: true, ballRerollCutoff: 151}}
 var gameSpecificCycleCounts = {RB: { reroll1: 520, reroll2: 564},
     Y: { reroll1: 516, reroll2: 560}}
 var roll2Cycles = 23664;
@@ -10,31 +6,23 @@ $('button').click(function () {
     var intendedRate = 0;
     var actualSuccesses = 0;
     var pokemon = JSON.parse($('#species').val());
-    var catchRate = pokemon.catchRate;
-    var baseHP = pokemon.baseHP;
+    var ball = JSON.parse($('#ball').val());
     var level = parseInt($('#level').val());
-    var ball = pokeballs[$('#ball').val()];
     var currentHPPercent = Math.max(parseInt($('#hpRange').val()), 1);
-    var statusLabel = $('#status').val();
-    var status = 0;
-    if (["poisoned", "burned", "paralyzed"].includes(statusLabel)) {
-        status = 12;
-    } else if (["asleep", "frozen"].includes(statusLabel)) {
-        status = 25;
-    }
+    var status = $('#status').val();
     var game = $('#game').val();
     var reroll1Cycles = gameSpecificCycleCounts[game].reroll1;
     var reroll2Cycles = gameSpecificCycleCounts[game].reroll2;
 
     for (var hpDV = 0; hpDV < 16; hpDV++) {
-        var maxHP = (((baseHP + hpDV) * 2 * level / 100) >> 0) + level + 10;
+        var maxHP = (((pokemon.baseHP + hpDV) * 2 * level / 100) >> 0) + level + 10;
         var hpFactor = (((maxHP * 255) / ball.ballFactor) >> 0);
         var currentHPModifier = (((maxHP * (currentHPPercent / 100)) >> 0) / 4) >> 0;
         if (currentHPModifier > 0) {
             hpFactor = (hpFactor / currentHPModifier) >> 0;
         }
         hpFactor = Math.min(hpFactor, 255);
-        intendedRate += status / ball.ballRerollCutoff + Math.min(catchRate + 1, ball.ballRerollCutoff - status) / ball.ballRerollCutoff * (hpFactor + 1) / 256;
+        intendedRate += status / ball.ballRerollCutoff + Math.min(pokemon.catchRate + 1, ball.ballRerollCutoff - status) / ball.ballRerollCutoff * (hpFactor + 1) / 256;
         for (var initialRNGByte = 0; initialRNGByte < 256; initialRNGByte++) {
             if (initialRNGByte < status) {
                 actualSuccesses += 16384;
@@ -59,7 +47,7 @@ $('button').click(function () {
                         }
                     }
                     while (reroll);
-                    if (currentRNGByte > catchRate) {
+                    if (currentRNGByte > pokemon.catchRate) {
                         catchMon = false;
                     } else {
                         currentDividerWord = (currentDividerWord + roll2Cycles) & 0xFFFF;
