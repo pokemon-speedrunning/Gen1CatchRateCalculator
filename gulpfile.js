f6e8de202d599ddf76e7cd400f185f708ed179e5
TiKevin83 <travismcgeehan@gmail.com>
2020-07-03 04:23:29 -0400

    use postcss-modules for bootstrap classnames

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,24 @@ const data = require('gulp-data');
 
 const sass = require('gulp-sass');
 const csso = require('gulp-csso');
+const postcss = require('gulp-postcss');
+const cssmodules = require('postcss-modules');
 
 const terser = require('gulp-terser-js');
 
 gulp.task('pages', function () {
+    const bootstrap = require('./src/css/modules/bootstrap.css.json');
+
     return gulp.src('src/html/pages/**/*.pug')
-        .pipe(data(function (file) { return { require: require }; }))
+        .pipe(data(function (file) {
+            let filePath = file.history[0].replace(file.base, '').split('.')[0].substr(1);
+            
+            return {
+                require: require,
+                pageName: filePath,
+                css: {bootstrap: bootstrap, local: require(`./src/css/${filePath}.scss.json`)}
+            };
+        }))
         .pipe(pug({}))
         .pipe(htmlmin({
             collapseWhitespace: true,
@@ -31,6 +43,7 @@ gulp.task('pages', function () {
 gulp.task('styles', function () {
     return gulp.src('src/css/*.scss')
         .pipe(sourcemaps.init())
+        .pipe(postcss([cssmodules({scopeBehaviour: "global", exportGlobals: true})]))
         .pipe(sass({
             outputStyle: 'nested',
             precision: 10,
@@ -41,6 +54,13 @@ gulp.task('styles', function () {
         .pipe(sourcemaps.write('../maps'))
         .pipe(gulp.dest('docs/css'));
 });
+
+gulp.task('globalStyleExport', function () {
+    return gulp.src('src/css/modules/*.css')
+        .pipe(postcss([cssmodules({scopeBehaviour: "global", exportGlobals: true})]));
+});
+
+gulp.task('styles', gulp.series('styles', 'globalStyleExport'));
 
 gulp.task('scripts', function () {
     return gulp.src('src/js/*.js')
@@ -58,11 +78,11 @@ gulp.task('clean', function () {
     return del(['docs']);
 });
 
-gulp.task('default', gulp.parallel('pages', 'scripts', 'styles'));
+gulp.task('default', gulp.series('styles','pages', 'scripts'));
 
 gulp.task('watch', function () {
     gulp.watch('src/html/**/*.pug', gulp.series('pages'));
     gulp.watch('src/data/**/*.json', gulp.series('pages'));
-    gulp.watch('src/css/**/*.scss', gulp.series('styles'));
+    gulp.watch('src/css/**/*.scss', gulp.series('styles', 'pages'));
     gulp.watch('src/js/**/*.js', gulp.series('scripts'));
 });
